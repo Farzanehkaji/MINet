dde4818925568f605db9695e33e945dcd79b23ed
Daoyi Wang <daoyiwang@Daoyis-MacBook-Pro.local>
2020-11-29 18:46:13 -0500

    translated comments in config.py

--- code/config.py
+++ code/config.py
@@ -16,16 +16,17 @@ dutstr_path = os.path.join(datasets_root, "DUTS/Train")
 dutste_path = os.path.join(datasets_root, "DUTS/Test")
 
 arg_config = {
-    "model": "MINet_VGG16",  # 实际使用的模型，需要在`network/__init__.py`中导入
-    "info": "",  # 关于本次实验的额外信息说明，这个会附加到本次试验的exp_name的结尾，如果为空，则不会附加内容。
-    "use_amp": False,  # 是否使用amp加速训练
-    "resume_mode": "",  # the mode for resume parameters: ['train', 'test', '']
-    "use_aux_loss": True,  # 是否使用辅助损失
-    "save_pre": True,  # 是否保留最终的预测结果
-    "epoch_num": 50,  # 训练周期, 0: directly test model
-    "lr": 0.001,  # 微调时缩小100倍
-    "xlsx_name": "result.xlsx",  # the name of the record file
-    # 数据集设置
+    "model": "MINet_VGG16",  # The netwotk model to be used. Need to import accordingly in 'network/__init__.py'
+    "info": "",  # You can include supplmentary descriptions. It will be attached to the end of the exp_name. If left empty, nothing will be attached 
+    "use_amp": False,  # Whether to enable AMP (Automatic Mixed Precision) to speed up training 
+    "resume_mode": "",  # The mode for resume parameters: ['train', 'test', '']
+    "use_aux_loss": True,  # Whether to enable uxiliary loss. If true, will use CEL in the training
+    "save_pre": True,  # Whether to save final prediction results
+    "epoch_num": 50,  # Number of epochs. Set to 0 means to test the model directly
+    "lr": 0.001,  # Learning rate. When fine-tuning, set to 1/100 of the original value
+    "xlsx_name": "result.xlsx",  # The name of the record file
+    
+    # Dataset settings
     "rgb_data": {
         "tr_data_path": dutstr_path,
         "te_data_list": OrderedDict(
@@ -39,26 +40,29 @@ arg_config = {
             },
         ),
     },
-    # 训练过程中的监控信息
-    "tb_update": 50,  # >0 则使用tensorboard
-    "print_freq": 50,  # >0, 保存迭代过程中的信息
-    # img_prefix, gt_prefix，用在使用索引文件的时候的对应的扩展名
+    
+    # Monitoring the training
+    "tb_update": 50,  # if >0, will use tensorboard
+    "print_freq": 50,  # >0, save iteration information
+    # img_prefix, gt_prefix, the suffix of image files and mask files, respectively
     "prefix": (".jpg", ".png"),
-    # if you dont use the multi-scale training, you can set 'size_list': None
+    # if you don't want to use the multi-scale training, you can set 'size_list': None
     # "size_list": [224, 256, 288, 320, 352],
-    "size_list": None,  # 不使用多尺度训练
-    "reduction": "mean",  # 损失处理的方式，可选“mean”和“sum”
-    # 优化器与学习率衰减
-    "optim": "sgd_trick",  # 自定义部分的学习率
-    "weight_decay": 5e-4,  # 微调时设置为0.0001
+    "size_list": None,  # Not using multi-scale training
+    "reduction": "mean",  # How to handle reduction,'mean' or 'sum'
+    
+    
+    # Optimizer and the learning rate decay
+    "optim": "sgd_trick",  # Customize the learning rate for part of the model
+    "weight_decay": 5e-4,  # When fine-tuning, set to 0.0001
     "momentum": 0.9,
     "nesterov": False,
     "sche_usebatch": False,
     "lr_type": "poly",
     "warmup_epoch": 1,  # depond on the special lr_type, only lr_type has 'warmup', when set it to 1, it means no warmup.
     "lr_decay": 0.9,  # poly
-    "use_bigt": True,  # 训练时是否对真值二值化（阈值为0.5）
-    "batch_size": 4,  # 要是继续训练, 最好使用相同的batchsize
-    "num_workers": 4,  # 不要太大, 不然运行多个程序同时训练的时候, 会造成数据读入速度受影响
+    "use_bigt": True,  # In the training, whether to binarize the ground truth image (threshold = 0.5)
+    "batch_size": 4,  # Keep the same batch_size when resuming a training
+    "num_workers": 4,  # If too big, it will impact the speed of data reading
     "input_size": 320,
 }
